30c4b237c0267f76e8db80af8aa9c25b1f843a94
Jonathan <jon.bluks@gmail.com>
2018-11-07 11:38:04 -0800

    Updates

---
+++ precache-manifest.ca279fc215ca92a94f56cbcb7c3d4582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "8c3d721a2a6f9981cde4",
+    "url": "/myapp/static/js/main.8c3d721a.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/myapp/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "8c3d721a2a6f9981cde4",
+    "url": "/myapp/static/css/main.e803aefe.chunk.css"
+  },
+  {
+    "revision": "089e1cece848cbab70f395faf0072387",
+    "url": "/myapp/index.html"
+  }
+];
